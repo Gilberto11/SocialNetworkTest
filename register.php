970d9f58ac504ae2d8b75e9cf3b6655c14a9e15b
gilberto <surfcorinthians@gmail.com>
2018-09-29 15:56:37 +0100

    scratch of registration form

---
+++ register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Snake Social Media</title>
+</head>
+<body>
+
+	<form action = "register.php" method="POST">
+		<input type= "text" name="reg_fname" placeholder="First Name" required>
+		<br>
+		<input type= "text" name="reg_lname" placeholder="Last Name" required>
+		<br>
+		<input type= "text" name="reg_email" placeholder="Email" required>
+		<br>
+		<input type= "text" name="reg_email2" placeholder="Confir Email" required>
+		<br>
+		<input type= "text" name="reg_password" placeholder="Password" required>
+		<br>
+		<input type= "text" name="reg_password2" placeholder="Confir Password" required>
+		<br>
+		<input type="submit" name="register_button" value = "Register">
+
+
+	</form>
+
+
+</body>
+</html>
