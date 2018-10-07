dbcb0dc4b322548d65484bbc6524068bba79da83
gilberto <surfcorinthians@gmail.com>
2018-10-07 09:51:25 +0100

    create form handlers

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 
 <?php 
-$con = mysqli_connect("localhost", "root", "", "snake");
-if(mysqli_connect_errno()){
-	echo "Failed to connect: " .mysqli_connect_errno();
-} 
+require 'config/config.php';
 
-$query = mysqli_query($con, "INSERT INTO accounts VALUES('1', 'Gilberto')");
-
-
- ?>
 <html>
 	<head>
 	
